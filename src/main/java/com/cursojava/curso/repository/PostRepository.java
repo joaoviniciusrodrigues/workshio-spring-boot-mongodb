c1c1fba49f5b8b2a88c0dfcc4fd024ddd56f308a
João Vinicius Rodrigues do Nascimento <joaoviniciusrodrigues@users.noreply.github.com>
2020-04-08 11:19:15 -0300

    Correão de bug na consulta filtrando por titulo

--- src/main/java/com/cursojava/curso/repository/PostRepository.java
+++ src/main/java/com/cursojava/curso/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.cursojava.curso.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -9,9 +10,12 @@ import com.cursojava.curso.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
-	@Query("{' title': { $regex: ?0, $options: 'i'} }")
+	@Query("{'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String title);
+	
+	//@Query("{ $and: [ {date: {$gte: ?1} }, {date: {$lte: ?2}}, { $or: [ {'title': { $regex: ?0, $options: 'i' } },{},{}]} ] }")
+	//List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
